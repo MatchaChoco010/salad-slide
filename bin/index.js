92129b7736567724e4390b7ed764e89d4ddab059
Orito Itsuki <20170107+MatchaChoco010@users.noreply.github.com>
2018-06-20 14:29:13 +0900

    Parse command line arguments

--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,21 @@
 #!/usr/bin/env node
-
 'use strict'
 
-console.log('Hi!')
+const path = require('path')
+const pkg = require('../package.json')
+const program = require('commander')
+
+let mdFilePath
+
+program
+  .version(pkg.version, '-v, --version')
+  .arguments('<*.md>')
+  .action(md => (mdFilePath = md))
+  .parse(process.argv)
+
+if (mdFilePath === undefined || path.parse(mdFilePath).ext !== '.md') {
+  console.error('no md file given!')
+  program.help()
+}
+
+console.log(mdFilePath)
