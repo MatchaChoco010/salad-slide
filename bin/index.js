7e3dfae0c32502fd6f90b856e037df702950e120
Orito Itsuki <20170107+MatchaChoco010@users.noreply.github.com>
2018-06-20 15:50:57 +0900

    Create tmp/index.html file and watching md file

--- bin/index.js
+++ bin/index.js
@@ -2,8 +2,13 @@
 'use strict'
 
 const path = require('path')
+const fs = require('fs')
+const pug = require('pug')
+const rimraf = require('rimraf')
 const pkg = require('../package.json')
 const program = require('commander')
+
+const tmpPath = path.join(__dirname, '../tmp/')
 
 let mdFilePath
 
@@ -18,4 +23,37 @@ if (mdFilePath === undefined || path.parse(mdFilePath).ext !== '.md') {
   program.help()
 }
 
-console.log(mdFilePath)
+if (!fs.existsSync(mdFilePath)) {
+  console.error('no such file!')
+  program.help()
+}
+
+const template = pug.compileFile(path.join(__dirname, '../src/template.pug'))
+
+const reloadMdFile = (eventType, _) => {
+  if (eventType === 'rename') {
+    console.error('Renamed or moved watching markdown file...')
+    process.exit(1)
+  }
+
+  if (eventType === 'change') {
+    console.log('markdown file is changed...')
+  }
+
+  const md = fs.readFileSync(mdFilePath, { encoding: 'utf8' })
+
+  rimraf.sync(tmpPath)
+  fs.mkdirSync(tmpPath)
+  fs.writeFileSync(
+    path.join(tmpPath, 'index.html'),
+    template({ markdown: md }),
+    {
+      encoding: 'utf8',
+    },
+  )
+}
+
+reloadMdFile()
+console.log(`Watching "${mdFilePath}"...`)
+
+fs.watch(mdFilePath, reloadMdFile)
